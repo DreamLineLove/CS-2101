5147d0a200d62daf7f2f66f77a62adca3e9de761
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-03 17:39:18 +0630

    Chapter 2 textbook ex3

---
+++ java/chapter2/textbook/ex3.java
@@ -0,0 +1,18 @@
+/*
+* Class Name: Testing
+* Description: A simple Java Program 
+*/
+
+class Testing {
+    public static void main (String args []) {
+        // main method:
+        
+        int a = 100; // variables
+        int b = 55;
+        int c = a - b; // calculate a - b
+        
+        System.out.println (a);
+        System.out.println (b);
+        System.out.println (c);
+    }
+}
