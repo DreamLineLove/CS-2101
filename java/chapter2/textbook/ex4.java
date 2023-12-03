c35eaa789ccbdda7708b65a88c09fcd426061642
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-03 17:39:43 +0630

    Chapter 2 textbook ex4

---
+++ java/chapter2/textbook/ex4.java
@@ -0,0 +1,9 @@
+class ex4 {
+    public static void main(String argv[]) {
+        System.out.println("\"Here i\"\n\"Here 2\"");
+        
+        String str = "x is ";
+        int x = 100;
+        System.out.println(str + x);
+    }
+}
