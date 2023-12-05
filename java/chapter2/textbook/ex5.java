3dc1f26eef3f035ed87596265cb14703f7d8d6b2
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-06 00:15:23 +0630

    update ex5

--- java/chapter2/textbook/ex5.java
+++ java/chapter2/textbook/ex5.java
@@ -1,12 +1,12 @@
 class ex5 {
     public static void main(String args[]) {
-        System.out.println("* * * *");
-        System.out.println("     *");
-        System.out.println("    *");
-        System.out.println("   *");
-        System.out.println("  *");
-        System.out.println(" *");
-        System.out.println("*");
-        System.out.println("* * * *");
+        System.out.println("* * * *\t\t*\t      *\t\t* * * *");
+        System.out.println("     *\t\t* *\t      *\t\t     *");
+        System.out.println("    *\t\t*   *\t      *\t\t    *");
+        System.out.println("   *\t\t*     *\t      *\t\t   *");
+        System.out.println("  *\t\t*       *     *\t\t  *");
+        System.out.println(" *\t\t*         *   *\t\t *");
+        System.out.println("*\t\t*           * *\t\t*");
+        System.out.println("* * * *\t        *\t      *\t        * * * *");
     }
 }
