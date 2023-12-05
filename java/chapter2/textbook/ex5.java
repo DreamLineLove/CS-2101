610e86a9f6396f6d628db58d64de462268707b66
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-05 23:56:20 +0630

    add ex5

---
+++ java/chapter2/textbook/ex5.java
@@ -0,0 +1,12 @@
+class ex5 {
+    public static void main(String args[]) {
+        System.out.println("* * * *");
+        System.out.println("     *");
+        System.out.println("    *");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*");
+        System.out.println("* * * *");
+    }
+}
