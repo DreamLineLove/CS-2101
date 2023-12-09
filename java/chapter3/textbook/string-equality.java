fbe16d3dbc2b09e9c3261d04a1407d2dcb679b24
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-09 20:47:57 +0630

    add more chapter 3

---
+++ java/chapter3/textbook/string-equality.java
@@ -0,0 +1,10 @@
+class StringEquality {
+    public static void main(String args[]) {
+        String str1 = "Here is a string";
+        String str2 = "Here is a string";
+
+        if (str1.equals(str2)) {
+            System.out.println("They are equal!");
+        }     
+    }
+}
