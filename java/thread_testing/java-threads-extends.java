e961836fd344944f6c4eb3cda2e53706d72b81f3
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-13 22:29:35 +0630

    java threads experiment furthered

---
+++ java/thread_testing/java-threads-extends.java
@@ -0,0 +1,32 @@
+class ThreadExtends extends Thread {
+    public ThreadExtends(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+
+        String name = Thread.currentThread().getName();
+
+        System.out.println("\tName = " + name);
+
+        try {
+            Thread.sleep(1000);
+            doStuff(name);
+            System.out.println();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.println();
+        }
+    }
+    
+    private void doStuff(String name) throws InterruptedException {
+       Thread.sleep(4000);
+
+       if (name != "java is the best!") { 
+           throw new InterruptedException("java is NOT the best?!!");
+       } else {
+           System.out.println("\n\tOfcourse java is the best!"); 
+        }
+    }
+}
