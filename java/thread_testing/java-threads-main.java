70040fc2655f38953817c04499396f50d5c1baa5
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-13 22:34:23 +0630

    java threads experiment furthered

--- java/thread_testing/java-threads-main.java
+++ java/thread_testing/java-threads-main.java
@@ -1,12 +1,22 @@
 class threads {
 
     public static void main(String[] args) {
-        Thread badRunnable = new Thread(new RunnableImplement(), "bad one");
+        // Custom class implements runnable interface first, then a new thread
+        // calls that runnable
+        Thread badRunnable = new Thread(new RunnableImplement(), "bad runnable");
         Thread goodRunnable = new Thread(new RunnableImplement(), "java is the best!");
 
-        System.out.println("Using runnable interface extension...");
+        System.out.println("Using runnable interface implementation...");
         badRunnable.start();
         goodRunnable.start();
+
+        // Creates custom class that extends the Thread class
+        Thread badThread = new ThreadExtends("bad thread");
+        Thread goodThread = new ThreadExtends("java is the best!");
+
+        System.out.println("Using thread class extension...");
+        badThread.start();
+        goodThread.start();
     }
 
 }
