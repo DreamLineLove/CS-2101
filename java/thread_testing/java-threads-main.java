cb9470ecf33a855ac1932e5548a6ac9c6901fff9
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-13 21:56:53 +0630

    java threads experiment furthered

---
+++ java/thread_testing/java-threads-main.java
@@ -0,0 +1,14 @@
+package threadTesting; 
+
+class Main {
+
+    public static void main(String[] args) {
+        Thread badRunnable = new Thread(new RunnableImplement(), "bad one");
+        Thread goodRunnable = new Thread(new RunnableImplement(), "java is the best!");
+
+        System.out.println("Using runnable interface extension...");
+        badRunnable.start();
+        goodRunnable.start();
+    }
+
+}
