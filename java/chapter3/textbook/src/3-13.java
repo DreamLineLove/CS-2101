adaf2946fb41f5ea10f2af4009b6c81773f73e7a
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-16 19:56:48 +0630

    new chapter3 hw

--- java/chapter3/textbook/src/3-13.java
+++ java/chapter3/textbook/src/3-13.java
@@ -5,6 +5,7 @@ class Ex3_13 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of x = ");
         int x = sc.nextInt();
         sc.close();
 
