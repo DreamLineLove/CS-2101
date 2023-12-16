6ae450242d184489ee58692102dafb53a3886a2f
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-16 20:36:26 +0630

    more chapter3 hw

--- java/chapter3/textbook/src/3-16.java
+++ java/chapter3/textbook/src/3-16.java
@@ -2,9 +2,11 @@ class Ex3_16 {
 
     public static void main(String[] args) {
 
-       int i = 10, j = 20, k = 7;
+        int i = 10, j = 20, k = 7;
 
-       System.out.println(k / i - j);
+        System.out.println(k / i - j);
+        System.out.println(i / (double)j);
+        System.out.println(i % (j + k));
 
     }
 
