649c13ed06b6378a8c71b4a94af15c4a329bf7f5
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-16 19:29:21 +0630

    more chapter3 hw

--- java/chapter3/textbook/src/3-12.java
+++ java/chapter3/textbook/src/3-12.java
@@ -8,6 +8,15 @@ class Ex3_12 {
         total = 5;
         currentValue = 5;
 
+        /*
+         * total / currentValue; is not a statement!
+         * the code therefore will error on compilation
+         *
+         * total / currentValue;
+         *
+         */
+
+        // the result of the division is assigned back to variable total
         total = total / currentValue;
         currentValue++;
 
