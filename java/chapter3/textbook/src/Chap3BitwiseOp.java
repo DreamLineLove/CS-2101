85c8c89eedf4bc392fc50790cdb08ba6a7063ba8
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-17 15:36:24 +0630

    chapter3 more hw

--- java/chapter3/textbook/src/Chap3BitwiseOp.java
+++ java/chapter3/textbook/src/Chap3BitwiseOp.java
@@ -2,7 +2,12 @@ class Chap3BitwiseOp {
 
     public static void main(String[] args) {
 
-                
+        int a = 5; // 0000 0101
+        int b = 14; // 0000 1110
+        int c = 0;
+
+        c = a & b; // decimal 4 or binary 0000 0100
+        System.out.println("Decimal c = " + c + "\t\tBinary c = " + Integer.toBinaryString(c));
 
     }
 
