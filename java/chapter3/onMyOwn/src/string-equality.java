498d114419ffdc3b35b417766f74abd63c6246aa
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-17 14:43:21 +0630

    new experiments

--- java/chapter3/onMyOwn/src/string-equality.java
+++ java/chapter3/onMyOwn/src/string-equality.java
@@ -3,8 +3,17 @@ class StringEquality {
         String str1 = "Here is a string";
         String str2 = "Here is a string";
 
-        if (str1.equals(str2)) {
-            System.out.println("They are equal!");
-        }     
+        String s1 = new String("Here is a string");
+        String s2 = new String("Here is a string");
+
+        // if (str1.equals(str2)) {
+        //     System.out.println("They are equal!");
+        // }     
+
+        if (str1 == str2) System.out.println("str1 == str2 works");
+        if (str1.equals(str2)) System.out.println("only str1.equals(str2) works");
+
+        if (s1 == s2) System.out.println("s1 == s2 works");
+        if (s1.equals(s2)) System.out.println("only s1.equals(s2) works");
     }
 }
