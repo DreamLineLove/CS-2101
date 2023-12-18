dab17f09c440377ff72dee708b6fc4f3366fce47
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-18 19:15:26 +0630

    add more chapter3 hw

---
+++ chapter3/textbook/src/Chap3Precedence.java
@@ -0,0 +1,17 @@
+class Chap3Precedence {
+
+    public static void main(String[] args) {
+
+        int a = 7; 
+        int b = 2; 
+        int c = 4; 
+        int d = 8; 
+        int e = 2; 
+
+        int result = a - b * c + d / e;
+
+        System.out.println("Value of a - b * c + d / e = " + result);
+
+    }
+
+}
