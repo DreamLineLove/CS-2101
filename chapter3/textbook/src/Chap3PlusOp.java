d33dda104ce1e0b9bae1f78f97aadbb2e1286059
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-18 19:00:08 +0630

    add more chapter3 hw

---
+++ chapter3/textbook/src/Chap3PlusOp.java
@@ -0,0 +1,14 @@
+class Chap3PlusOp {
+
+    public static void main(String[] args) {
+
+        String part1 = "Learn to Program";
+        String part2 = ": ";
+        String part3 = "Java";
+        String title = part1 + part2;
+        title += part3;
+        System.out.println("The title of this book is " + title);
+
+    }
+
+}
