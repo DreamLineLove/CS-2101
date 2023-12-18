a1590ccf3c1597d1adc87d5abf44446454af35b2
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-18 17:56:51 +0630

    no-inline-comments branch preparation

--- chapter3/textbook/README.md
+++ chapter3/textbook/README.md
@@ -5,5 +5,35 @@
 total  currentValue; is not a statement!
 Therefore, `total currentValue;` will error upon compilation.
 
-As for `total = total / currentValue;`, the result of the division is assigned 
+As for `total = total  currentValue;`, the result of the division is assigned 
 back to variable total.
+
+## Chap3BitwiseOp
+
+```java
+c = ~a; 
+```
+Binary 1111 1010 starts with negative bit (first bit 1). Therefore it is a 
+negative number.
+         
+Negative numbers are represented by taking the two's complement of the original 
+number.
+         
+To find out what 1111 1010 is the negative of, we have to find out the original 
+number first. 
+         
+To do so, first turn it back to one's complement form, i.e. 1111 1001, and then 
+negate the one's complement, 0000 0110, which is 6 in decimal.
+         
+Therefore, c is negative 6 or -6.
+
+```java
+c = ~a >>> 2; 
+```
+Taking the complement of a results in a number starting with 1 (negative). 
+It is then shifted two positions to the right disregarding sign.
+(Meaning the leftmost 2 bits are now filled with 0).
+         
+This means the resulting number is a very huge positive number, since c is 
+defined as an int (32-bits)
+
--- chapter3/textbook/src/Chap3BitwiseOp.java
+++ chapter3/textbook/src/Chap3BitwiseOp.java
@@ -15,22 +15,7 @@ class Chap3BitwiseOp {
         c = a | b; // decimal 15 or binary 0000 1111
         System.out.println("Decimal c = " + c + "\t\tBinary c = " + Integer.toBinaryString(c));
 
-        c = ~a; 
-        /* 
-         * binary 1111 1010 starts with negative bit (first bit 1). Therefore it
-         * is a negative number.
-         *
-         * Negative numbers are represented by taking the two's complement of the
-         * original number.
-         *
-         * To find out what 1111 1010 is the negative of, we have to find out the
-         * original number first. 
-         *
-         * To do so, first turn it back to one's complement form, i.e. 1111 1001,
-         * and then negate the one's complement, 0000 0110, which is 6 in decimal.
-         *
-         * Therefore, c is negative 6 or -6.
-        */
+        c = ~a; // decimal -6 or binary 1111 1010
         System.out.println("Decimal c = " + c + "\t\tBinary c = " + Integer.toBinaryString(c));
 
         c = b >> 2; // decimal 3 or binary 0000 0011
@@ -43,14 +28,6 @@ class Chap3BitwiseOp {
         System.out.println("Decimal c = " + c + "\t\tBinary c = " + Integer.toBinaryString(c));
 
         c = ~a >>> 2; 
-        /*
-         * Taking the complement of a results in a number starting with 1 (negative).
-         * It is then shifted two positions to the right disregarding sign.
-         * (Meaning the leftmost 2 bits are now filled with 0).
-         *
-         * This means the resulting number is a very huge positive number, since
-         * c is defined as an int (32-bits)
-        */
         System.out.println("Decimal c = " + c + "\tBinary c = " + Integer.toBinaryString(c));
         
     }
