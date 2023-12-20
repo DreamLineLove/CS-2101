89ac7c54923980ab0d44b8a5d8a18d1c307b1b9d
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-20 22:10:07 +0630

    add chapter4 hw

---
+++ chapter4/textbook/src/Chap4Block.java
@@ -0,0 +1,19 @@
+class Chap4Block {
+
+    public static void main(String args[]) {
+
+        int a = 5;
+        int b = 6;
+        int c = a + b;
+
+        if (c > 10) {
+            System.out.println("c is greater than 10");
+            c = 0;
+            System.out.println("c is now " + c);
+        } else {
+            System.out.println("c is less than 10" + c);
+        }
+
+    }
+
+}
