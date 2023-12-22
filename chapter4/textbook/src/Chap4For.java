0c009474da0d39ed406ad513f1786f91c88a686a
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-22 22:56:21 +0630

    add more chapter4 hw

---
+++ chapter4/textbook/src/Chap4For.java
@@ -0,0 +1,10 @@
+class Chap4For {
+
+    public static void main(String[] args) {
+
+        for (int number = 0; number < 10; number++) System.out.println(number);
+        System.out.println("Out of for statement");
+
+    }
+
+}
