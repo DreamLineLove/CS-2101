03bd1a553453d0501f1367c67b4a2a8f301a54d2
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-22 23:27:24 +0630

    add more chapter4 hw

---
+++ chapter4/textbook/src/Chap4BreakOuter.java
@@ -0,0 +1,31 @@
+class Chap4BreakOuter {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = new int[10][10];
+        for (int i = 1; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                matrix[i][j] = (i * 10) + j;
+                System.out.println(matrix[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nNumbers divislbe by 3 are:");
+
+        outer:
+        for (int i = 1; i < 10; i++) {
+             (int j = 0; j < 10; j++) {
+                if (matrix[i][j] % 3 == 0) {
+                    System.out.println(matrix[i][j] + " ");
+                    break outer;
+                }
+            }
+            System.out.println();
+            System.out.println("Out of inner for loop");
+        }
+        System.out.println("Out of outer for loop");
+
+    }
+
+}
