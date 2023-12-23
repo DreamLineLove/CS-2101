decdb261de62b898362962c47d1370a347dfecb8
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-23 15:29:42 +0630

    add more chapter4 hw

---
+++ chapter4/textbook/src/Chap4CopyArray.java
@@ -0,0 +1,25 @@
+class Chap5CopyArray {
+
+    public static void main(String argv[]) {
+
+        int sourceMarks[] = new int[5];
+        sourceMarks[0] = 89;
+        sourceMarks[1] = 56;
+        sourceMarks[2] = 88;
+        sourceMarks[3] = 35;
+        sourceMarks[4] = 65;
+
+        int targetMarks[] = {30, 40, 50, 60, 70, 80};
+        
+        int i = 0;
+
+        System.out.println("Before copying arrays");
+        for (i = 0; i < sourceMarks.length; i++) {
+            System.out.print("sourceMarks[" + i + "]=" + sourceMarks[i]);
+            System.out.println(" targetMarks[" + i + "]=" + targetMarks[i]);
+        }
+        System.out.println();
+
+    }
+
+}
