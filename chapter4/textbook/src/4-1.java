4f2e2c92007abfa55d5dec3a7ec3b6ec93c0d9ae
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-23 16:24:45 +0630

    add more chapter4 hw

---
+++ chapter4/textbook/src/4-1.java
@@ -0,0 +1,49 @@
+class Ex4_1a {
+
+    private static void run() {
+
+        boolean cond1 = true, cond2 = true, cond3 = false;     
+
+        if (cond1 != false) 
+        System.out.println("@@@@@");
+        if (cond2) 
+        System.out.println("#####");
+        else
+        System.out.println("$$$$$"); // Guess: will not get printed
+        if (cond3) 
+        System.out.println("&&&&&");
+        System.out.println("*****");
+
+    }
+
+}
+
+class Ex4_1b {
+
+    private static void run() {
+
+                
+
+    }
+
+}
+
+class Ex4_1c {
+
+    private static void run() {
+
+                
+
+    }
+
+}
+
+class Ex4_1 {
+
+    public static void main(String[] args) {
+
+        Ex4_1a.run();
+
+    }
+
+}
