6b93627f81f0372f545b0ee2a1f1539106e212ba
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-24 16:24:16 +0630

    add more chapter4 hw

---
+++ chapter4/textbook/src/4-11.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Ex4_11 {
+
+    public static void main(String[] args) {
+
+        int x, y;
+        double power;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of x: ");
+        x = sc.nextInt();
+
+        System.out.print("Enter value of x: ");
+        y = sc.nextInt();
+
+        sc.close();
+
+        power = power(x, y);
+        System.out.println(x + " power " + y + " = " + power);
+
+    }
+
+    static double power(int x, int y) {
+        double result = 1;
+
+        for (int i = 0; i < absolute(y); i++) {
+            result = result * absolute(y);
+        }
+
+        if (y < 0) {
+            return 1 / result;
+        } else {
+            return result;
+        }
+    }
+
+    static int absolute(int y) {
+        if (y < 0) return -y;
+        else return y;
+    }
+
+}
