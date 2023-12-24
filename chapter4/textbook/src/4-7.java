94c61737efbe18367e3afdd1cc1556b4f7aaa67e
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-24 15:40:23 +0630

    add more chapter4 hw

--- chapter4/textbook/src/4-7.java
+++ chapter4/textbook/src/4-7.java
@@ -3,7 +3,7 @@ class Ex4_7 {
     public static void main(String[] args) {
 
         for (int i = 5; i >= -5; i--) {
-            System.out.println(i + " ");
+            System.out.print(i + " ");
         }
 
     }
