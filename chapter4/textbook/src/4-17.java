38c1d24163d8ed064d51b23b5dcef30ef11c6c7a
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-25 15:39:02 +0630

    more chapter4

--- chapter4/textbook/src/4-17.java
+++ chapter4/textbook/src/4-17.java
@@ -1,14 +1,22 @@
 class Ex4_17 {
+    
+    static int N;
 
     public static void main(String[] args) {
 
-        int N = (int)(Math.random() * 9);
-
-        for (int i = 0; i < 50; i++) {
-            if (i % 10 == 0) System.out.println();
-            System.out.print(N + " ");
-        }
+        N = (int)(Math.random() * 9) + 1;
+        recursive();
 
     } 
+
+    static void recursive() {
+        if (N == 1) System.out.println(N);
+        else {
+            System.out.println(N);
+            if (N % 2 == 0) N = N / 2;
+            else N = (N * 3) + 1;
+            recursive(N);
+        }
+    }
 
 }
