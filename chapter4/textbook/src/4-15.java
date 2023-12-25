9cc6a48d7fc6c2237f7957c3e945b88415af302e
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-25 15:08:54 +0630

    more chapter4

---
+++ chapter4/textbook/src/4-15.java
@@ -0,0 +1,14 @@
+class Ex4_15 {
+
+    public static void main(String[] args) {
+
+        int numOfAsterisk = Integer.parseInt(args[0]);
+
+        for (int i = 0; i < numOfAsterisk; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+    }
+
+}
