0e3a9ba6dff37823055f7de4ee28c25b4c7d861d
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-28 00:33:59 +0630

    more problem-solving from chapter4

---
+++ chapter4/textbook/src/4-33.java
@@ -0,0 +1,16 @@
+class Ex4_33 {
+
+    public static void main(String[] args) {
+
+        int list[] = {3, 4, 5, 6, 7};
+        int sum = 0;
+
+        for (int i = 0; i < list.length; i++) {
+            sum += list[i];
+        }
+        
+        System.out.println("Sum of list = " + sum);
+
+    }
+
+}
