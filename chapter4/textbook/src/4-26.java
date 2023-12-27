caeb95c757f0f4f4212e9dfa997a73d0c1c9ee1a
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-27 23:15:54 +0630

    more problem-solving from chapter4

---
+++ chapter4/textbook/src/4-26.java
@@ -0,0 +1,22 @@
+class Ex4_26 {
+
+    public static void main(String[] args) {
+
+        approximateValueOfPi(10);
+
+    }
+
+    static void approximateValueOfPi(int n) {
+        double approximation = 0;
+
+        for (int i = 1; i <= n; i++) {
+            approximation += calculateNthTerm(i);
+            System.out.println("Approximate value of pi by " + i + " term(s) = " + approximation);
+        }
+    }
+
+    static double calculateNthTerm(int n) {
+        return (n % 2 == 0) ? (-4 / 2 * n - 1) : (4 / 2 * n - 1);
+    }
+
+}
