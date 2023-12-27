16bb5261f5f7c32beca654a16b68ee137a2ad973
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-28 00:39:47 +0630

    more problem-solving from chapter4

--- chapter4/textbook/src/4-34.java
+++ chapter4/textbook/src/4-34.java
@@ -2,14 +2,14 @@ class Ex4_34 {
 
     public static void main(String[] args) {
 
-        int list[] = {-3, 4, -5, 6, -7};
+        int list[] = {-3, 4, -2, 6, -9};
 
         for (int i = 0; i < list.length; i++) {
             if (list[i] < 0) list[i] = -(list[i]);
         }
         
         for (int i = 0; i < list.length; i++) {
-            System.out.println(i);
+            System.out.println(list[i]);
         }
     }
 
