b8e12dbaf6e5cabfa371ca1785cc51e993326836
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-28 00:02:10 +0630

    more problem-solving from chapter4

---
+++ chapter4/textbook/src/4-31.java
@@ -0,0 +1,15 @@
+class Ex4_31 {
+
+    public static void main(String[] args) {
+
+        String [] arr = new String[5];
+        for (int i = 0; i < arr.length; i++) 
+            System.out.println(arr[i]);
+
+        // Prediction
+        // It will print "null" 5 times.
+        // Uninitialized object references in Java supposedly evaluate to null.
+
+    }
+
+}
