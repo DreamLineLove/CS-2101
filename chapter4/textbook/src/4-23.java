7209a01daa8f6e8d11b651a824eaee3e9e524347
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-27 20:03:31 +0630

    more problem-solving from chapter4

--- chapter4/textbook/src/4-23.java
+++ chapter4/textbook/src/4-23.java
@@ -4,7 +4,7 @@ class Ex4_23 {
 
         int highest = 0;
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 1; i <= 5; i++) {
             int num = (int)(Math.random() * 16) + 15;
             System.out.println("Number " + i + " : " + num);
             if (highest < num) highest = num;
