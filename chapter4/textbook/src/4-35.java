96813a8ab28bd018f354f9bf063de1b76c4d1fdb
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-28 21:53:30 +0630

    more problem-solving from chapter4

--- chapter4/textbook/src/4-35.java
+++ chapter4/textbook/src/4-35.java
@@ -14,7 +14,7 @@ class Ex4_35 {
     static void display(int matrix[][]) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j]);
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
