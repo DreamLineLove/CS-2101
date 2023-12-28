b76cd761ca9eb598275a23169b1d6ac67b1d528c
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-28 22:41:46 +0630

    more problem-solving from chapter4

---
+++ chapter4/textbook/src/4-40.java
@@ -0,0 +1,14 @@
+class Ex4_40 {
+
+    public static void main(String[] args) {
+
+        int i = 7, j = 0;
+        if (++i > 7) // if 8 > 7
+        ++j; // j is now 1
+        j++; // j becomes 2 after this.
+
+        System.out.println(i + " " + j);
+
+    }
+
+}
