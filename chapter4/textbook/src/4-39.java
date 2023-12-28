e54facb10e3c2a01f2931b96dd71c244eb175ca7
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-28 22:33:50 +0630

    more problem-solving from chapter4

---
+++ chapter4/textbook/src/4-39.java
@@ -0,0 +1,12 @@
+class Ex4_39 {
+
+    public static void main(String[] args) {
+
+        int j = 0;
+        for (int i = 0; i < 10; i += 3) 
+            j += i;
+        System.out.println(i + " " + j); // Guess -> i + j = 30
+
+    }
+
+}
