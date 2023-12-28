1c0f5f9a4cf11987b0f31610c7fcdc0c1ac5660d
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-28 22:45:31 +0630

    more problem-solving from chapter4

---
+++ chapter4/textbook/src/4-41.java
@@ -0,0 +1,20 @@
+class Ex4_40 {
+
+    public static void main(String[] args) {
+        int i = 7, j = 0;
+        if (i > 0)
+        if (i < 7)
+        j = 1;
+        else
+        j = 2;
+        
+        System.out.println(j);
+
+        // If i is greater than 0, execute the following:
+        //      if i is less than 7, j is reassigned 1;
+        //      else j is reassigned 2;
+        //
+        // In this case, j gets reassigned 2
+    }
+
+}
