359046162e95a582e326105d3a230868312ea9d6
DreamLineLove <zwenyanzaw@protonmail.com>
2023-12-28 22:35:42 +0630

    more problem-solving from chapter4

--- chapter4/textbook/src/4-39.java
+++ chapter4/textbook/src/4-39.java
@@ -2,8 +2,16 @@ class Ex4_39 {
 
     public static void main(String[] args) {
 
-        int j = 0;
-        for (int i = 0; i < 10; i += 3) 
+        // int j = 0;
+        // for (int i = 0; i < 10; i += 3) 
+        //     j += i;
+        // System.out.println(i + " " + j); 
+        //
+        // The above code does not even compile because i is not declared in the
+        // bigger scope.
+        
+        int i = 0, j = 0;
+        for (i = 0; i < 10; i += 3) 
             j += i;
         System.out.println(i + " " + j); // Guess -> i + j = 30
 
