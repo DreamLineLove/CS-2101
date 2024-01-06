cba3f8c764169efd3cf3e8ad03479f488aa47348
DreamLineLove <zwenyanzaw@protonmail.com>
2024-01-06 23:17:47 +0630

    wrote a new assignment 'Volume' and improved upon the error-catching while loop logic from 'Area'

--- projects/src/Area.java
+++ projects/src/Area.java
@@ -22,35 +22,28 @@ class Area {
         System.out.print("\nChoose shape.. ");
         shape = sc.nextInt();
 
-        while (true) {
-            try {
-                switch (shape) {
-                    case 0:
-                    calculateArea(shapes[0].toLowerCase(), "Area of circle = πr^2", sc, "radius");
-                    break;
-                    case 1:
-                    calculateArea(shapes[1].toLowerCase(), "Area of square = length * length", sc, "side length");
-                    break;
-                    case 2:
-                    calculateArea(shapes[2].toLowerCase(), "Area of triangle = 1/2 (base * height)", sc, "base", "height");
-                    break;
-                    case 3:
-                    calculateArea(shapes[3].toLowerCase(), "Area of rectangle = length * width", sc, "length", "width");
-                    break;
-                    case 4:
-                    calculateArea(shapes[4].toLowerCase(), "Area of parallelogram = base * height", sc, "base", "height");
-                    break;
-                    case 5:
-                    calculateArea(shapes[5].toLowerCase(), "Area of trapezoid = ( (a + b) / 2 ) * height", sc, "a", "b", "height");
-                    break;
-                    case 6:
-                    calculateArea(shapes[6].toLowerCase(), "Area of ellipse = πab", sc, "a", "b");
-                    break;
-                }
-                break;
-            } catch (InputMismatchException e ) {
-                System.out.println("The input must be numerical values.\n");
-            }
+        switch (shape) {
+            case 0:
+            calculateArea(shapes[0].toLowerCase(), "Area of circle = πr^2", sc, "radius");
+            break;
+            case 1:
+            calculateArea(shapes[1].toLowerCase(), "Area of square = length * length", sc, "side length");
+            break;
+            case 2:
+            calculateArea(shapes[2].toLowerCase(), "Area of triangle = 1/2 (base * height)", sc, "base", "height");
+            break;
+            case 3:
+            calculateArea(shapes[3].toLowerCase(), "Area of rectangle = length * width", sc, "length", "width");
+            break;
+            case 4:
+            calculateArea(shapes[4].toLowerCase(), "Area of parallelogram = base * height", sc, "base", "height");
+            break;
+            case 5:
+            calculateArea(shapes[5].toLowerCase(), "Area of trapezoid = ( (a + b) / 2 ) * height", sc, "a", "b", "height");
+            break;
+            case 6:
+            calculateArea(shapes[6].toLowerCase(), "Area of ellipse = πab", sc, "a", "b");
+            break;
         }
 
         sc.close();
@@ -59,36 +52,42 @@ class Area {
     static void calculateArea(String shape, String formula, Scanner sc, String... terms) throws InputMismatchException {
         double []valueOfTerms = new double[terms.length];
         surround(formula);
-        try {
-            for (int i = 0; i < terms.length; i++) {
-                System.out.print("Enter value of " + terms[i] + "\t");
-                valueOfTerms[i] = sc.nextDouble();
+        while (true) {
+            try {
+                for (int i = 0; i < terms.length; i++) {
+                    System.out.print("Enter value of " + terms[i] + "\t");
+                    valueOfTerms[i] = sc.nextDouble();
+                }
+                switch (shape) {
+                    case "circle":
+                    System.out.println("- Area of circle = " + Math.PI * valueOfTerms[0] * valueOfTerms[0]);
+                    break;
+                    case "square":
+                    System.out.println("- Area of square = " + valueOfTerms[0] * valueOfTerms[0]);
+                    break;
+                    case "triangle":
+                    System.out.println("- Area of triangle = " + ((valueOfTerms[0] * valueOfTerms[1]) / 2));
+                    break;
+                    case "rectangle":
+                    System.out.println("- Area of rectangle = " + valueOfTerms[0] * valueOfTerms[1]);
+                    break;
+                    case "parallelogram":
+                    System.out.println("- Area of parallelogram = " + valueOfTerms[0] * valueOfTerms[1]);
+                    break;
+                    case "trapezoid":
+                    System.out.println("- Area of trapezoid = " + ((valueOfTerms[0] + valueOfTerms[1]) / 2) * valueOfTerms[2]);
+                    break;
+                    case "ellipse":
+                    System.out.println("- Area of ellipse = " + Math.PI * valueOfTerms[0] * valueOfTerms[1]);
+                    break;
+                }
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("! The input must be numerical values.\n");
+                sc.nextLine();
             }
-        } catch (InputMismatchException e) {}
-        switch (shape) {
-            case "circle":
-                System.out.println("- Area of circle = " + Math.PI * valueOfTerms[0] * valueOfTerms[0]);
-                break;
-            case "square":
-                System.out.println("- Area of square = " + valueOfTerms[0] * valueOfTerms[0]);
-                break;
-            case "triangle":
-                System.out.println("- Area of triangle = " + ((valueOfTerms[0] * valueOfTerms[1]) / 2));
-                break;
-            case "rectangle":
-                System.out.println("- Area of rectangle = " + valueOfTerms[0] * valueOfTerms[1]);
-                break;
-            case "parallelogram":
-                System.out.println("- Area of parallelogram = " + valueOfTerms[0] * valueOfTerms[1]);
-                break;
-            case "trapezoid":
-                System.out.println("- Area of trapezoid = " + ((valueOfTerms[0] + valueOfTerms[1]) / 2) * valueOfTerms[2]);
-                break;
-            case "ellipse":
-                System.out.println("- Area of ellipse = " + Math.PI * valueOfTerms[0] * valueOfTerms[1]);
-                break;
         }
-    }
+   }
 
     static void surround(String s) {
         for (int i = 0; i < s.length() + 4; i++) {
---
+++ projects/src/Volume.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+class Volume {
+
+    public static void main(String argv[]) {
+        int shape;
+        String []shapes = {
+            "Sphere",
+            "Cube",
+            "Triangular Prism",
+            "Cuboid",
+            "Cylinder",
+            "Cone",
+            "Pyramid",
+        };
+
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < shapes.length; i++) {
+            System.out.println(i + "\t" + shapes[i]);
+        }
+        System.out.print("\nChoose shape.. ");
+        shape = sc.nextInt();
+
+        switch (shape) {
+            case 0:
+            calculateVolume(shapes[0].toLowerCase(), "Volume of sphere = 4/3 πr^3", sc, "radius");
+            break;
+            case 1:
+            calculateVolume(shapes[1].toLowerCase(), "Volume of cube = s^3", sc, "side length");
+            break;
+            case 2:
+            calculateVolume(shapes[2].toLowerCase(), "Volume of triangular prism = 1/2 bhl", sc, "base", "height", "length between the two triangles");
+            break;
+            case 3:
+            calculateVolume(shapes[3].toLowerCase(), "Volume of cuboid = lwh", sc, "length", "width", "height");
+            break;
+            case 4:
+            calculateVolume(shapes[4].toLowerCase(), "Volume of cylinder = πr^2h", sc, "radius of the base", "height");
+            break;
+            case 5:
+            calculateVolume(shapes[5].toLowerCase(), "Volume of cone = 1/3 πr^2h", sc, "radius of the base", "height");
+            break;
+            case 6:
+            calculateVolume(shapes[6].toLowerCase(), "Volume of pyramid = 1/3 lwh", sc, "length", "width", "height");
+            break;
+        }
+
+        sc.close();
+    }
+
+    static void calculateVolume(String shape, String formula, Scanner sc, String... terms) throws InputMismatchException {
+        double []valueOfTerms = new double[terms.length];
+        surround(formula);
+        while (true) {
+            try {
+                for (int i = 0; i < terms.length; i++) {
+                    System.out.print("Enter value of " + terms[i] + "\t");
+                    valueOfTerms[i] = sc.nextDouble();
+                }
+                switch (shape) {
+                    case "sphere":
+                    System.out.println("- Volume of sphere = " + (Math.PI * valueOfTerms[0] * valueOfTerms[0] * valueOfTerms[0] * 4) / 3);
+                    break;
+                    case "cube":
+                    System.out.println("- Volume of cube = " + valueOfTerms[0] * valueOfTerms[0] * valueOfTerms[0]);
+                    break;
+                    case "triangular prism":
+                    System.out.println("- Volume of triangular prism = " + (valueOfTerms[0] * valueOfTerms[1] * valueOfTerms[2]) / 2);
+                    break;
+                    case "cuboid":
+                    System.out.println("- Volume of cuboid = " + valueOfTerms[0] * valueOfTerms[1] * valueOfTerms[2]);
+                    break;
+                    case "cylinder":
+                    System.out.println("- Volume of cylinder = " + Math.PI * valueOfTerms[0] * valueOfTerms[0] * valueOfTerms[1]);
+                    break;
+                    case "cone":
+                    System.out.println("- Volume of cone = " + (Math.PI * valueOfTerms[0] * valueOfTerms[0] * valueOfTerms[1]) / 3);
+                    break;
+                    case "pyramid":
+                    System.out.println("- Volume of pyramid = " + (valueOfTerms[0] * valueOfTerms[1] * valueOfTerms[2]) / 3);
+                    break;
+                }
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("! The input must be numerical values.\n");
+                sc.nextLine();
+            }
+        }
+    }
+
+    static void surround(String s) {
+        for (int i = 0; i < s.length() + 4; i++) {
+            System.out.print("-");
+        }
+        System.out.println("\n| " + s + " |");
+        for (int i = 0; i < s.length() + 4; i++) {
+            System.out.print("-");
+        }
+        System.out.println("\n");
+    }
+}
