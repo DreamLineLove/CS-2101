49b990225eae8df00d62ce60c08376ae5442f2eb
DreamLineLove <zwenyanzaw@protonmail.com>
2024-03-15 23:08:37 +0630

    change chapter5 ex7 and 8

--- chapter5/textbook/5-7.java
+++ chapter5/textbook/5-7.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 class Ex5_7 {
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int num1 = Integer.parseInt(args[0]);
-        int num2 = Integer.parseInt(args[1]);
+        System.out.print("Enter int1: ");
+        int num1 = sc.nextInt();
+        
+        System.out.print("Enter int2: ");
+        int num2 = sc.nextInt();
+
         System.out.println(gcd(num1, num2));
 
+        sc.close();
     }
 
     static int gcd(int num1, int num2) {
@@ -20,5 +27,4 @@ class Ex5_7 {
 
         return gcd;
     }
-
 }
--- chapter5/textbook/5-8.java
+++ chapter5/textbook/5-8.java
@@ -1,16 +1,22 @@
+import java.util.Scanner;
+
 class Ex5_8 {
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int num1 = Integer.parseInt(args[0]);
-        int num2 = Integer.parseInt(args[1]);
+        System.out.print("Enter int1: ");
+        int num1 = sc.nextInt();
+        
+        System.out.print("Enter int2: ");
+        int num2 = sc.nextInt();
+
         System.out.println(gcd(num1, num2));
 
+        sc.close();
     }
 
     static int gcd(int num1, int num2) {
         if (num2 == 0) return num1;
         else return gcd(num2, num1 % num2);
     }
-
 }
