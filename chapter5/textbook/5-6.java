f2057af0336323b9410a9c5b91c3be199e76577d
DreamLineLove <zwenyanzaw@protonmail.com>
2024-03-15 23:27:01 +0630

    change chapter5 ex6 and 9

--- chapter5/textbook/5-6.java
+++ chapter5/textbook/5-6.java
@@ -1,15 +1,21 @@
+import java.util.Scanner;
+
 class Ex5_6 {
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int hour = Integer.parseInt(args[0]);
-        int minute = Integer.parseInt(args[1]);
-        System.out.println(convertToSecond(hour, minute));
+        System.out.print("Enter hours: ");
+        int hour = sc.nextInt();
+        
+        System.out.print("Enter minutes: ");
+        int minute = sc.nextInt();
 
+        System.out.println("\n" + convertToSecond(hour, minute) + " seconds");
+
+        sc.close();
     }
 
     static int convertToSecond(int hour, int minute) {
         return hour * 3600 + minute * 60;
     }
-
 }
--- chapter5/textbook/5-9.java
+++ chapter5/textbook/5-9.java
@@ -1,20 +1,22 @@
 class Ex5_9 {
-
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide integers as program arguments separated by space!");
+            System.out.println("for example, 5 6 7 12 215 25 12");
+        } else {
+            int[] arr = new int[args.length];
 
-        int []arr = new int[args.length];
+            for (int i = 0; i < args.length; i++) {
+                int element = Integer.valueOf(args[i]);
+                arr[i] = element;
+                System.out.println(element);
+            }
 
-        for (int i = 0; i < args.length; i++) {
-            int element = Integer.valueOf(args[i]);
-            arr[i] = element;
-            System.out.println(element);
+            System.out.println("\nSmallest = " + smallestNumber(arr));
         }
-
-        System.out.println("\nSmallest = " + smallestNumber(arr));
-
     }
 
-    static int smallestNumber(int arr[]) {
+    static int smallestNumber(int[] arr) {
         int smallest = arr[0];
 
         for (int i = 1; i < arr.length; i++) {
@@ -23,5 +25,4 @@ class Ex5_9 {
 
         return smallest;
     }
-
 }
